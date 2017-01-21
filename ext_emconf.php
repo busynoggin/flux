e6974348158a59b83f60b02358911b2c9ba1cf50
Jeff Segars <jeff@busynoggin.com>
2017-01-21 16:02:25 -0600

    Revert "[REMOVAL] Drop support for PHP 5.x (#1276)"
    This reverts commit f4cd720f2a88b696ea0b9c9295a8628433b8938e.
    
    # Conflicts:
    #	ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'typo3' => '7.6.13-8.6.99',
-      'php' => '7.0.0-7.1.99',
+      'php' => '5.5.0-7.1.99',
     ),
     'conflicts' =>
     array (

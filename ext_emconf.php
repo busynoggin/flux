2d90876ce43eb127b7e4fcb57987d42158478e0d
Jeff Segars <jeff@busynoggin.com>
2017-01-21 16:02:25 -0600

    Revert "[REMOVAL] Drop support for PHP 5.x (#1276)"
    This reverts commit f4cd720f2a88b696ea0b9c9295a8628433b8938e.
    
    # Conflicts:
    #	ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,22 +21,22 @@ $EM_CONF[$_EXTKEY] = array (
   'author_company' => '',
   'CGLcompliance' => '',
   'CGLcompliance_note' => '',
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
       'typo3' => '7.6.13-8.5.99',
-      'php' => '7.0.0-7.1.99',
+      'php' => '5.5.0-7.1.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
   '_md5_values_when_last_written' => '',
-  'suggests' => 
+  'suggests' =>
   array (
   ),
 );
